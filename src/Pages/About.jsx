154488b714fc663f829b843c9e20281c4a68d54e
Krishan Goyal <krishangoyal9350@gmail.com>
2024-06-01 14:40:56 +0530

    commit

---
+++ src/Pages/About.jsx
@@ -0,0 +1,73 @@
+import { IconPhone } from "@tabler/icons-react";
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPages";
+import PlanTrip from "../components/PlanTrip";
+import AboutMain from "../images/about/about-main.jpg";
+import Box1 from "../images/about/icon1.png";
+import Box2 from "../images/about/icon2.png";
+import Box3 from "../images/about/icon3.png";
+
+function About() {
+  return (
+    <>
+      <section className="about-page">
+        <HeroPages name="About" />
+        <div className="container">
+          <div className="about-main">
+            <img
+              className="about-main__img"
+              src={AboutMain}
+              alt="car-renting"
+            />
+            <div className="about-main__text">
+              <h3>About Company</h3>
+              <h2>You start the engine and your adventure begins</h2>
+              <p>
+              CAR Rental is the leading marketplace for car sharing in emerging markets,with over 2,000 cars on its technology-driven platform across India. It empowers host entrepreneurs to safely and easily share their cars to earn additional passive income. Guests in the CAR Rental's community enjoy a diverse, affordable selection of cars to unlock memorable driving experiences with friends and family.  
+              </p>
+              <div className="about-main__text__icons">
+                <div className="about-main__text__icons__box">
+                  <img src={Box1} alt="car-icon" />
+                  <span>
+                    <h4>20</h4>
+                    <p>Car Types</p>
+                  </span>
+                </div>
+                <div className="about-main__text__icons__box">
+                  <img src={Box2} alt="car-icon" />
+                  <span>
+                    <h4>85</h4>
+                    <p>Rental Outlets</p>
+                  </span>
+                </div>
+                <div className="about-main__text__icons__box">
+                  <img src={Box3} alt="car-icon" className="last-fk" />
+                  <span>
+                    <h4>75</h4>
+                    <p>Repair Shop</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <PlanTrip />
+        </div>
+      </section>
+      <div className="book-banner">
+        <div className="book-banner__overlay"></div>
+        <div className="container">
+          <div className="text-content">
+            <h2>Book a car by getting in touch with us</h2>
+            <span>
+              <IconPhone width={40} height={40} />
+              <h3>+91 1234567890</h3>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default About;
